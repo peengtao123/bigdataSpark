1edc76cdc0ad0dc83e0e56955bc5533da6379183
pengtao <you@example.com>
2019-01-23 15:59:28 +0800

    init

--- src/main/java/org/core/WordCountJava.java
+++ src/main/java/org/core/WordCountJava.java
@@ -1,21 +1,18 @@
 package org.core;
 
 import org.apache.spark.SparkConf;
-import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
-import org.apache.spark.sql.SparkSession;
-import scala.Tuple2;
 
 import java.util.Arrays;
 
 public class WordCountJava {
-    public static void main(String[] args){
-        SparkConf conf = new SparkConf().setAppName("wordCoutn").setMaster("local");
+    public static void main(String[] args) {
+        SparkConf conf = new SparkConf().setAppName("wordcount").setMaster("local[*]");
         JavaSparkContext sc = new JavaSparkContext(conf);
 
-        JavaRDD<String> text = sc.textFile("hdfs://hadoop:9000/data/dataset");
-        text.foreach(x->System.out.println(x));
+        JavaRDD<String> text = sc.textFile("data");
+        text.flatMap(x-> Arrays.asList(x.split(" ")).iterator()).foreach(x -> System.out.println(x));
 
         sc.stop();
     }
